f326dbafbd760cd8c5d49bad6935e89b875210dd
Rupam Dhar <rupam@promact.com>
2018-02-09 16:37:49 +0530

    Second branch commit

---
+++ Equalitycheck/Equalitycheck/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equalitycheck
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            float X= 0.96f;
+            float Y = 5.94f;
+            float sum = X + Y;
+            Console.WriteLine("The sum is:" + " " + sum);
+            int a = 6;
+            Console.WriteLine("The sum is equal to 6 ?" + ReferenceEquals(sum, a));
+            bool itfComp = ((IComparable<float>)sum == (IComparable<int>)a);
+            Console.WriteLine("The sum is equal to 6 ?" + itfComp);
+            float result = X > Y ? X : Y;
+            Console.WriteLine("The greater no is:" + result);
+     
+            
+
+        }
+    }
+}
---
+++ Inventory/Inventory/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> invent = new List<string>();
+            invent.Add("Tomato");
+            invent.Add("Potato");
+            invent.Add("ToMato");
+            invent.Add("Potato");
+            invent.Add("Red Tomato");
+            invent.Add("Tomato Catchup");
+            invent.Add("Grapes");
+            invent.Add("grapes");
+            invent.Add("potato");
+            var pro = invent.ConvertAll(d => d.ToLower());
+            int count = 0;
+            foreach (var p in pro)
+            {
+                if (p=="tomato")
+                    count++;
+            }
+            Console.WriteLine("The no of strings containing tomato"+" "+count);
+            Console.WriteLine("The indexes are:" + invent.FindIndex(collection => collection.SequenceEqual("Potato")) + " " + invent.FindLastIndex(collection => collection.SequenceEqual("Potato")));
+        }
+    }
+}
